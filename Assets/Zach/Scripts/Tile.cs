e909b9f4d98b78fadb9a616e8cf04f777d412c92
Zachary Flores <zaflores@usc.edu>
2018-08-31 18:06:01 -0700

    Tile for sterling <3

---
+++ Assets/Zach/Scripts/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]
+    private int type;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetType(int type)
+    {
+        this.type = type;
+    }
+
+    public void GetType()
+    {
+
+    }
+}
