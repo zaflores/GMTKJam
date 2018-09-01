0b5c0ab05eba8b64b411322edaf919ce6ebbf713
Zachary Flores <zaflores@usc.edu>
2018-09-01 09:53:23 -0700

    Match 3 mechanics are done!

---
+++ Assets/Zach/Scripts/BoardManager.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+
+    [SerializeField] private Sprite[] _gameSprites;
+    [SerializeField] private GameObject _titleSmallPrefab;
+    [SerializeField] private GameObject _titleNormalPrefab;
+    [SerializeField] private int _xTitles;
+    [SerializeField] private int _yTitles;
+    [SerializeField] private Vector2 _startPosForGameBoard;
+    private int[,] masterBoard;
+    private List<GameObject> holdingMasterObjects;
+    private GameObject[,] gameBoard;
+    private int[] numOfEachSprite;
+
+    public GameObject[,] getBoard()
+    {
+        return gameBoard;
+    }
+
+    public void StartBoardGame()
+    {
+        holdingMasterObjects = new List<GameObject>();
+        numOfEachSprite = new int[_gameSprites.Length];
+        masterBoard = new int[_xTitles, _yTitles];
+        gameBoard = new GameObject[_xTitles, _yTitles];
+        Vector2 offset = _titleSmallPrefab.GetComponent<SpriteRenderer>().bounds.size;
+        CreateInitialBoard(offset.x + 0.5f, offset.y + 0.5f);
+        offset = _titleNormalPrefab.GetComponent<SpriteRenderer>().bounds.size;
+        CreateGameBoard(offset.x + 0.5f, offset.y + 0.5f);
+    }
+
+    public int CheckForThrees()
+    {
+        int numThrees = 0;
+        //check for vertical 3's
+        for (int i = 0; i < _xTitles; i++)
+        {
+            for (int j = 1; j < _yTitles - 1; j++)
+            {
+                Tile tempTile1 = gameBoard[i, j].GetComponent<Tile>();
+                Tile tempTile2 = gameBoard[i, j + 1].GetComponent<Tile>();
+                Tile tempTile3 = gameBoard[i, j - 1].GetComponent<Tile>();
+                if (tempTile1.GetNumberType() == tempTile2.GetNumberType() &&
+                    tempTile1.GetNumberType() == tempTile3.GetNumberType())
+                {
+                    numThrees++;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < _yTitles; i++)
+        {
+            for (int j = 1; j < _xTitles - 1; j++)
+            {
+                Tile tempTile1 = gameBoard[j, i].GetComponent<Tile>();
+                Tile tempTile2 = gameBoard[j + 1, i].GetComponent<Tile>();
+                Tile tempTile3 = gameBoard[j - 1, i].GetComponent<Tile>();
+                if (tempTile1.GetNumberType() == tempTile2.GetNumberType() &&
+                    tempTile1.GetNumberType() == tempTile3.GetNumberType())
+                {
+                    numThrees++;
+                    break;
+                }
+            }
+        }
+        return numThrees;
+    }
+    public bool CheckForWin()
+    {
+        for (int i = 0; i < _xTitles; i++)
+        {
+            for (int j = 0; j < _yTitles; j++)
+            {
+                Tile tempTile = gameBoard[i, j].GetComponent<Tile>();
+                if (tempTile.GetNumberType() != masterBoard[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void ClearGame()
+    {
+        ClearGameBoard();
+        ClearMasterBoard();
+    }
+
+	private void Start ()
+	{
+	    StartBoardGame();
+	}
+
+    private void CreateInitialBoard(float offsetX, float offsetY)
+    {
+        Vector2 startingPos = transform.position;
+        int[] previousRow = new int[_yTitles];
+        int[] previousPreviousRow = new int[_yTitles];
+        
+        for (int i = 0; i < _xTitles; i++)
+        {
+            int underType = -1;
+            int underUnderType = -1;
+            for (int j = 0; j < _yTitles; j++)
+            {
+                int[] availableToChoose = new int[_gameSprites.Length];
+                Vector2 positionToSpawn = new Vector2(startingPos.x + offsetX * i, startingPos.y + offsetY * j);
+                GameObject title = Instantiate(_titleSmallPrefab, positionToSpawn,Quaternion.identity);
+                holdingMasterObjects.Add(title);
+                title.transform.parent = transform;
+                if (i > 1)
+                {
+                    if(previousRow[j] == previousPreviousRow[j])
+                        availableToChoose[previousRow[j]] = -1;
+                }
+                if (j > 1)
+                {
+                    if (underType == underUnderType)
+                        availableToChoose[underType] = -1;
+                }
+                int spriteToChoose = Random.Range(0, _gameSprites.Length);
+                while (availableToChoose[spriteToChoose] == -1)
+                {
+                    spriteToChoose = Random.Range(0, _gameSprites.Length);
+                }
+                masterBoard[i, j] = spriteToChoose;
+                title.GetComponent<SpriteRenderer>().sprite = _gameSprites[spriteToChoose];
+                numOfEachSprite[spriteToChoose]++;
+                underUnderType = underType;
+                underType = spriteToChoose;
+                previousPreviousRow[j] = previousRow[j];
+                previousRow[j] = spriteToChoose;
+            }
+        }
+    }
+
+    private void CreateGameBoard(float offsetX, float offsetY)
+    {
+        for (int i = 0; i < _xTitles; i++)
+        {
+            for (int j = 0; j < _yTitles; j++)
+            {
+                Vector2 positionToSpawn = new Vector2(_startPosForGameBoard.x + offsetX * i, _startPosForGameBoard.y + offsetY * j);
+                GameObject title = Instantiate(_titleNormalPrefab, positionToSpawn, Quaternion.identity);
+                gameBoard[i, j] = title;
+                title.transform.parent = transform;
+                int spriteToChoose = Random.Range(0, _gameSprites.Length );
+                while (numOfEachSprite[spriteToChoose] == 0)
+                {
+                    spriteToChoose = Random.Range(0,numOfEachSprite.Length);
+                }
+                
+                title.GetComponent<SpriteRenderer>().sprite = _gameSprites[spriteToChoose];
+                Tile temptile = title.GetComponent<Tile>();
+                temptile.SetCoordinate(i,j);
+                temptile.SetType(spriteToChoose);
+                numOfEachSprite[spriteToChoose]--;
+            }
+        }
+    }
+
+   
+
+    private  void ClearGameBoard()
+    {
+        for (int i = 0; i < _xTitles; i++)
+        {
+            for (int j = 0; j < _yTitles; j++)
+            {
+                Destroy(gameBoard[i,j]);
+                gameBoard[i, j] = null;
+            }
+        }
+    }
+
+    private void ClearMasterBoard()
+    {
+        for (int i = 0; i < _xTitles; i++)
+        {
+            for (int j = 0; j < _yTitles; j++)
+            {
+                masterBoard[i, j] = -1;
+                GameObject temp = holdingMasterObjects[0];
+                holdingMasterObjects.RemoveAt(0);
+                Destroy(temp);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ClearGame();
+            StartBoardGame();
+        }
+    }
+}
--- Assets/Zach/Scripts/Tile.cs
+++ Assets/Zach/Scripts/Tile.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using DG.Tweening;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public static bool tileAlreadySelected = false;
+    public static GameObject previousTileSelected;
     [SerializeField]
     private int type;
+
+    private int xCoord;
+    private int yCoord;
+
+    public bool isSelected = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -24,5 +33,244 @@ public class Tile : MonoBehaviour
     public int GetNumberType()
     {
         return this.type;
+    }
+
+    public void SetCoordinate(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    public Vector2 GetCoordinate()
+    {
+        return new Vector2(xCoord,yCoord);
+    }
+
+    private void OnMouseDown()
+    {
+        if (isSelected)
+        {
+            previousTileSelected = null;
+            tileAlreadySelected = false;
+            isSelected = false;
+            transform.localScale -= new Vector3(.2f,.2f,0);
+        }
+        else
+        { 
+            if (tileAlreadySelected && previousTileSelected != gameObject)
+            {
+                if(CheckForValidSwap(gameObject,previousTileSelected))
+                {
+                    Swap(gameObject,previousTileSelected);
+                    BoardManager manager = GameObject.FindWithTag("Board Manager").GetComponent<BoardManager>();
+                    manager.CheckForThrees();
+                    if (manager.CheckForWin())
+                    {
+                        //win
+                    }
+                    tileAlreadySelected = false;
+                    previousTileSelected.transform.localScale -= new Vector3(.2f, .2f, 0);
+                    Tile tempTile = previousTileSelected.GetComponent<Tile>();
+                    tempTile.isSelected = false;
+                    previousTileSelected = null;
+                }
+                else
+                {
+                    Tile otherSelected = previousTileSelected.GetComponent<Tile>();
+                    previousTileSelected.transform.localScale -= new Vector3(.2f, .2f, 0);
+                    otherSelected.isSelected = false;
+                    previousTileSelected = gameObject;
+                    transform.localScale += new Vector3(.2f, .2f, 0);
+                    isSelected = true;
+                }
+            }
+            else
+            {
+                tileAlreadySelected = true;
+                previousTileSelected = gameObject;
+                isSelected = true;
+                transform.localScale += new Vector3(.2f, .2f, 0);
+            }
+           
+        }
+    }
+
+    private bool CheckForValidSwap(GameObject go1, GameObject go2)
+    {
+      
+        Tile tile1 = go1.GetComponent<Tile>();
+        Tile tile2 = go2.GetComponent<Tile>();
+        Vector2 tile1Coordinate = tile1.GetCoordinate();
+        Vector2 tile2Coordinate = tile2.GetCoordinate();
+        int x1 = (int)tile1Coordinate.x;
+        int y1 = (int)tile1Coordinate.y;
+        int x2 = (int)tile2Coordinate.x;
+        int y2 = (int)tile2Coordinate.y;
+        //vertical
+        if (x1 == x2)
+        {
+            GameObject boardmanagerobject = GameObject.FindWithTag("Board Manager");
+            BoardManager boardManager = boardmanagerobject.GetComponent<BoardManager>();
+            GameObject[,] board = boardManager.getBoard();
+            bool[] hasType = new bool[4];
+            for (int i = Mathf.Min(y1, y2); i <= Mathf.Max(y1, y2); i++)
+            {
+                Tile tempTile = board[x1, i].GetComponent<Tile>();
+                int tempType = tempTile.GetNumberType();
+                if (hasType[tempType])
+                {
+                    return false;
+                }
+                else
+                {
+                    hasType[tempType] = true;
+                }
+            }
+            return true;
+        }
+        //horizontal
+        else if (y1 == y2)
+        {
+            GameObject boardmanagerobject = GameObject.FindWithTag("Board Manager");
+            BoardManager boardManager = boardmanagerobject.GetComponent<BoardManager>();
+            GameObject[,] board = boardManager.getBoard();
+            bool[] hasType = new bool[4];
+            for (int i = Mathf.Min(x1, x2); i <= Mathf.Max(x1, x2); i++)
+            {
+                Tile tempTile = board[i, y1].GetComponent<Tile>();
+                int tempType = tempTile.GetNumberType();
+                if (hasType[tempType])
+                {
+                    return false;
+                }
+                else
+                {
+                    hasType[tempType] = true;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private void Swap(GameObject go1, GameObject go2)
+    {
+        Tile tempTile1 = go1.GetComponent<Tile>();
+        Tile tempTile2 = go2.GetComponent<Tile>();
+        GameObject boardmanagerobject = GameObject.FindWithTag("Board Manager");
+        BoardManager boardManager = boardmanagerobject.GetComponent<BoardManager>();
+        GameObject[,] board = boardManager.getBoard();
+        Vector2 tempCoordinate1 = tempTile1.GetCoordinate();
+        Vector2 tempCoordinate2 = tempTile2.GetCoordinate();
+        int x1 = (int)tempCoordinate1.x;
+        int y1 = (int)tempCoordinate1.y;
+        int x2 = (int)tempCoordinate2.x;
+        int y2 = (int) tempCoordinate2.y;
+        if (x1 == x2)
+        {
+            int diff = Mathf.Abs(y1 - y2);
+            switch (diff)
+            {
+                case 1:
+                    GameObject tempObj = go1;
+                    board[x1, y1] = go2;
+                    board[x2, y2] = tempObj;
+                    tempTile1.SetCoordinate(x2,y2);
+                    tempTile2.SetCoordinate(x1,y1);
+                    Vector3 origPosition1 = go1.transform.position;
+                    go1.transform.DOMove(go2.transform.position,.3f);
+                    go2.transform.DOMove(origPosition1, .3f);
+                    break;
+                case 2:
+                    tempObj = go1;
+                    board[x1, y1] = go2;
+                    board[x2, y2] = tempObj;
+                    tempTile1.SetCoordinate(x2, y2);
+                    tempTile2.SetCoordinate(x1, y1);
+                    origPosition1 = go1.transform.position;
+                    go1.transform.DOMove(go2.transform.position, .3f);
+                    go2.transform.DOMove(origPosition1, .3f);
+                    break;
+                case 3:
+                    //swap outside 2
+                    tempObj = go1;
+                    board[x1, y1] = go2;
+                    board[x2, y2] = tempObj;
+                    tempTile1.SetCoordinate(x2, y2);
+                    tempTile2.SetCoordinate(x1, y1);
+                    origPosition1 = go1.transform.position;
+                    go1.transform.DOMove(go2.transform.position, .3f);
+                    go2.transform.DOMove(origPosition1, .3f);
+                    //swapInside 2
+                    int min = Mathf.Min(y1, y2);
+                    tempObj = board[x1, min + 1];
+                    board[x1, min + 1] = board[x1, min + 2];
+                    board[x1, min + 2] = tempObj;
+                    tempTile1 = board[x1, min + 2].GetComponent<Tile>();
+                    tempTile2 = board[x1, min + 1].GetComponent<Tile>();
+                    tempTile1.SetCoordinate(x1,min + 2);
+                    tempTile2.SetCoordinate(x1, min + 1);
+                    origPosition1 = board[x1, min + 2].transform.position;
+                    board[x1, min + 2].transform.DOMove(board[x1, min + 1].transform.position, .3f);
+                    board[x1, min + 1].transform.DOMove(origPosition1, .3f);
+                    break;
+                default:
+                    return;
+            }
+        }
+        else
+        {
+            int diff = Mathf.Abs(x1 - x2);
+            switch (diff)
+            {
+                case 1:
+                    GameObject tempObj = go1;
+                    board[x1, y1] = go2;
+                    board[x2, y2] = tempObj;
+                    tempTile1.SetCoordinate(x2, y2);
+                    tempTile2.SetCoordinate(x1, y1);
+                    Vector3 origPosition1 = go1.transform.position;
+                    go1.transform.DOMove(go2.transform.position, .3f);
+                    go2.transform.DOMove(origPosition1, .3f);
+                    break;
+                case 2:
+                    tempObj = go1;
+                    board[x1, y1] = go2;
+                    board[x2, y2] = tempObj;
+                    tempTile1.SetCoordinate(x2, y2);
+                    tempTile2.SetCoordinate(x1, y1);
+                    origPosition1 = go1.transform.position;
+                    go1.transform.DOMove(go2.transform.position, .3f);
+                    go2.transform.DOMove(origPosition1, .3f);
+                    break;
+                case 3:
+                    //move outside 2
+                    tempObj = go1;
+                    board[x1, y1] = go2;
+                    board[x2, y2] = tempObj;
+                    tempTile1.SetCoordinate(x2, y2);
+                    tempTile2.SetCoordinate(x1, y1);
+                    origPosition1 = go1.transform.position;
+                    go1.transform.DOMove(go2.transform.position, .3f);
+                    go2.transform.DOMove(origPosition1, .3f);
+                    //move middle 2
+                    int min = Mathf.Min(x1, x2);
+                    tempObj = board[min+1, y1];
+                    board[min+1, y1] = board[min + 2, y1];
+                    board[min + 2, y1] = tempObj;
+                    tempTile1 = board[min + 2, y1].GetComponent<Tile>();
+                    tempTile2 = board[min+1, y1].GetComponent<Tile>();
+                    tempTile1.SetCoordinate(min + 2, y1);
+                    tempTile2.SetCoordinate(min+1, y1);
+                    origPosition1 = board[min + 2, y1].transform.position;
+                    board[min + 2, y1].transform.DOMove(board[min+1, y1].transform.position, .3f);
+                    board[min+1, y1].transform.DOMove(origPosition1, .3f);
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 }
