649dae17ed39efc320083f38b375c92ea78aa926
STERLING-PC\Sterling <sterlinr@usc.edu>
2018-08-31 23:55:14 -0700

    end of day 1

---
+++ Assets/Sterling/revolt.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class revolt : MonoBehaviour {
+
+    public int rows;
+    public int cols;
+    public GameObject[,] allObjects;
+    public Sprite[] rioters;
+
+    public float animationTime = 1;
+    private float timeTracker = 0;
+
+    public GameObject revoltImage;
+
+    private bool revolting = false;
+
+    public void startRiot()
+    {
+        //make all people rioters
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                allObjects[i, j].GetComponent<SpriteRenderer>().sprite = rioters[0];
+            }
+        }
+
+        Instantiate(revoltImage, allObjects[rows/2, cols/2].transform);
+
+        revolting = true;
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (revolting)
+        {
+            //animation for rioters
+            timeTracker -= Time.deltaTime;
+
+            if (timeTracker <= 0)
+            {
+                timeTracker = animationTime;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (allObjects[i, j].GetComponent<SpriteRenderer>().sprite == rioters[0])
+                        {
+
+                            allObjects[i, j].GetComponent<SpriteRenderer>().sprite = rioters[1];
+                        }
+
+                        else
+                        {
+                            allObjects[i, j].GetComponent<SpriteRenderer>().sprite = rioters[0];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Sterling/riotText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class riotText : MonoBehaviour {
+
+    public float growthRate;
+    private float growthTimer;
+    public float maxTime;
+    public int spinSpeed;
+    bool growing = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (growthTimer >= maxTime)
+        {
+            growing = false;
+        }
+
+        else if (growthTimer <= 0)
+        {
+            growing = true;
+        }
+
+        if (growthTimer < maxTime && growing)
+        {
+            transform.localScale += new Vector3(growthRate * Time.deltaTime, growthRate * Time.deltaTime, 0);
+            growthTimer += Time.deltaTime;
+        }
+
+        else
+        {
+            transform.localScale -= new Vector3(growthRate * Time.deltaTime, growthRate * Time.deltaTime, 0);
+            growthTimer -= Time.deltaTime;
+        }
+      
+        gameObject.GetComponent<Transform>().Rotate(Vector3.back * Time.deltaTime * spinSpeed);
+    }
+}
